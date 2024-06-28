370815b81218776674ddebcd15f1daeece64b211
ericung <ungericw@tutanota.com>
2024-06-28 16:41:36 -0500

    js simple

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+var editor = document.getElementById("editor");
+var pseudocode = document.getElementById("pseudocode");
 
+if (editor.addEventListener) {
+	editor.addEventListener('input', function() {
+		pseudocode.value = editor.value;
+		pseudocode.innerText = editor.value;
+	});
+}
+else if (editor.attachEvent) {
+	editor.attachEvent('onpropertychange', function() {
+		pseudocode.value = editor.value;
+		pseudocode.innerText = editor.value;
+	});
+}
