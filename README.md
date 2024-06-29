9ad22911f44291b9c9c74bf17e5ac9ef150d7f88
ericung <ungericw@tutanota.com>
2024-06-29 08:36:12 -0500

    simple regex parser

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# onlinelatexalgorithmeditor
+# online latex algorithm editor
+
+```
+\State i = 0
+\If i = 1 to 3
+\Else print "hello world"
+\EndIf
+```
+
+prints
+
+```
+i = 0
+If i = 1 to 3
+Else print "hello world"
+EndIf
+```
--- index.js
+++ index.js
@@ -3,13 +3,47 @@ var pseudocode = document.getElementById("pseudocode");
 
 if (editor.addEventListener) {
 	editor.addEventListener('input', function() {
-		pseudocode.value = editor.value;
-		pseudocode.innerText = editor.value;
+		var val = parser(editor.value);
+		pseudocode.value = val;
+		pseudocode.innerText = val;
 	});
 }
 else if (editor.attachEvent) {
 	editor.attachEvent('onpropertychange', function() {
-		pseudocode.value = editor.value;
-		pseudocode.innerText = editor.value;
+		var val = parser(editor.value);
+		pseudocode.value = val;
+		pseudocode.innerText = val;
 	});
+}
+
+const STATE = /\\State/g
+const IF = /\\If/g 
+const ELSE = /\\Else/g 
+const ELSIF = /\\ElsIf/g 
+const ENDIF = /\\EndIf/g
+
+function parser(text) {
+	var lines = text.split('\n');
+	var result = "";
+
+	for(var i = 0; i < lines.length; i++)
+	{
+		if (lines[i].match(STATE)) {
+			result += lines[i].split('\\State')[1] + "\n";
+		}
+		else if (lines[i].match(IF)){
+			result += "If " + lines[i].split('\\If')[1] + "\n";
+		}
+		else if (lines[i].match(ELSE)){
+			result += "Else " + lines[i].split('\\Else')[1] + "\n";
+		}
+		else if (lines[i].match(ELSIF)){
+			result += "ElseIf " + lines[i].split('\\ElsIf')[1] + "\n";
+		}
+		else if (lines[i].match(ENDIF)){
+			result += "EndIf\n";
+		}
+	}
+
+	return result;
 }
